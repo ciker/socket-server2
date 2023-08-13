25268512265275f83e583188cd849ac0c0208c14
Matheus Matsukawa <matheus.matsukawa@calindra.com.br>
2023-08-13 18:56:40 -0300

    refactor: remove unused muts

--- src/connection_handler.rs
+++ src/connection_handler.rs
@@ -1,15 +1,14 @@
-#![allow(unused_mut)]
 use crate::joker::joker;
 use crate::messager::Messager;
 use std::net::{SocketAddr, TcpStream};
 
-pub fn connection_handler(mut stream: TcpStream) {
+pub fn connection_handler(stream: TcpStream) {
     let adress: SocketAddr = stream.peer_addr().unwrap();
     println!("New connection: {}", &adress);
 
-    let mut buffer: [u8; 1024] = [0; 1024];
+    let buffer: [u8; 1024] = [0; 1024];
 
-    let mut messager = Messager::new(stream, buffer);
+    let messager = Messager::new(stream, buffer);
 
     joker(messager);
 }
